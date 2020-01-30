f69ed59ff551e8815c8c4110e5ed347ad15cb308
Taesthetic <mohammed.faryssy@etu.univ-amu.fr>
2020-01-30 15:06:48 +0100

    tri fusion working

--- src/FusionSort.java
+++ src/FusionSort.java
@@ -3,13 +3,13 @@ import java.util.List;
 
 public class FusionSort<T extends Comparable> implements Sort<T> {
 
-    private List<T> list;
+    private LinkedList<T> list;
 
     public FusionSort(List<T> list) {
-        this.list = list;
+        this.list = new LinkedList<>(list);
     }
 
-    public List<T> getList() {
+    public LinkedList<T> getList() {
         return list;
     }
 
@@ -22,51 +22,32 @@ public class FusionSort<T extends Comparable> implements Sort<T> {
     }
 
     private LinkedList<T> merge(LinkedList<T> list1, LinkedList<T> list2) {
-        int max = Math.max(list1.size(), list2.size());
-        int min = Math.min(list1.size(), list2.size());
+        LinkedList<T> retList = new LinkedList<>();
+        while (0 < list1.size() && 0 < list2.size()) {
+            if (list1.peekFirst().compareTo(list2.peekFirst()) <= 0)
+                retList.add(list1.pollFirst());
+            if (list1.peekFirst().compareTo(list2.peekFirst()) >= 0)
+                retList.add(list2.pollFirst());
 
-        aux2(list1, list2);
-
-    }
-
-    private T getMin(LinkedList<T> list) {
-        T min = list.pollFirst();
-        for (T element: list) {
-            if (element.compareTo(min) < 0) min = element;
         }
-        return min;
-    }
-    private LinkedList<T> aux2(LinkedList<T> list1, LinkedList<T> list2 ) {
-        while (list1.size() > 0 && list2.size() > 0) {
-            LinkedList<T> temp_list = new LinkedList<>();
-            if (getMin(list1).compareTo(getMin(list2)) < 0) {
-                temp_list.add(getMin(list1));
-                list1.removeFirstOccurrence(getMin(list1));
-            } else if (getMin(list1).compareTo(getMin(list2)) > 0) {
-                temp_list.add(getMin(list2));
-                list1.removeFirstOccurrence(getMin(list2));
-            }
 
-            else {
-                temp_list.add(getMin(list1));
-                list1.removeFirstOccurrence(getMin(list1));
-                temp_list.add(getMin(list2));
-                list1.removeFirstOccurrence(getMin(list2));
-            }
+        if (!list1.isEmpty())
+            retList.addAll(list1);
+        if (!list2.isEmpty())
+            retList.addAll(list2);
 
-            return temp_list;
-        }
+        return retList;
     }
 
-
-
-        private LinkedList<T> aux(LinkedList<T> list) {
-        if (list.size() <= 1)
+    private LinkedList<T> mergeSort(LinkedList<T> list) {
+        if (list.size() < 2)
             return list;
-        return merge(aux(divide_left(list)), aux(divide_right(list)));
+        LinkedList<T> leftList = mergeSort(divide_left(list));
+        LinkedList<T> rightList = mergeSort(divide_right(list));
+        return merge(leftList, rightList);
     }
 
     public void apply() {
-        list = aux(new LinkedList<>(list));
+        list = mergeSort(list);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -8,21 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> testList = new ArrayList<>(Arrays.asList(10, -4, 2, -3));
-        List<Integer> testList2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        //LinkedList<Integer> testList2 = new LinkedList<>(Arrays.asList(1, 2, 3, 4));
 
-        System.out.println(Utils.isSorted(testList));
+        /*System.out.println(Utils.isSorted(testList));
         System.out.println(Utils.isSorted2(testList));
         System.out.println(Utils.isSorted(testList2));
-        System.out.println(Utils.isSorted2(testList2));
+        System.out.println(Utils.isSorted2(testList2));*/
 
         //for (int value : testList)
         //    System.out.print(" " + value);
 
         System.out.println(testList);
+        //System.out.println(new FusionSort(testList).getMin(new LinkedList(testList)));
         //new BubbleSort().applyTo(testList);
-        Sort fus = new FusionSort(testList);
-        fus.apply();
-        System.out.println(fus.getList());
+        Sort merge = new FusionSort(testList);
+        merge.apply();
+        System.out.println(merge.getList());
         //System.out.println(testList.subList(0, 2));
     }
 }
